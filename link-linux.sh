af33735fe95ff001457d125ad99a589f0fa03c27
ssaito <shinya.saito.25a@gmail.com>
2016-04-08 22:51:33 +0900

    [変更]:.ubuntuから.linuxへいくつかのファイル名を変更

---
+++ link-linux.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+#
+#
+############################################################
+
+# for zsh
+ln -s ~/dotfiles/zsh/.zshrc ~/.zshrc
+ln -s ~/dotfiles/zsh/.zshrc.linux ~/.zshrc.local
+
+# for emacs
+ln -s ~/dotfiles/.emacs.d ~/.emacs.d
+
+# for tmux
+ln -s ~/dotfiles/tmux/.tmux.conf ~/.tmux.conf
+ln -s ~/dotfiles/tmux/.tmux.conf.linux ~/.tmux.conf.local 
