dba3333b0deeecbf248e49dc9670844f3ae69a5e
ssaito <shinya.saito.25a@gmail.com>
2019-04-25 07:26:22 +0900

    [add]:81-oas-yaml.elを追加

---
+++ .emacs.d/inits/81-oas-yaml.el
@@ -0,0 +1,9 @@
+;;; package --- Open API Spec のメジャーモードの設定
+;;; Commentary:
+;;;  yaml-mode
+
+;;; Code:
+
+; パッケージの指定
+(use-package openapi-yaml-mode
+  :straight (:host github :repo "magoyette/openapi-yaml-mode"))
